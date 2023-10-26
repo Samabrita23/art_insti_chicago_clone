a0aa605214379b9dde8810ac69f0ab2eb3f76d22
Samabrita <samabritabiswas2302@gmail.com>
2023-10-26 16:52:54 +0530

    update 1

--- src/Components/HomepageHeader.tsx
+++ src/Components/HomepageHeader.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import RedLogo from "../Assets/RedLogo.svg";
 import HomePageBackground from "../Assets/HomePageBackground.svg";
 import "../Styles/HomepageHeader.css";
+import { FaSearch } from "react-icons/fa";
 
 
 const HomepageHeader = () => {
@@ -11,7 +12,7 @@ const HomepageHeader = () => {
              <img
               src={HomePageBackground} 
               alt ="A person sits at a table in a study-like room stringing objects—flowers, leaves, vegetables, crystals, scraps of paper with mathematical equations—onto a three-dimensional musical staff.Figures emerging from the wall do the same, while the floor's tiles come loose, drawers overflow, and birds nest in the back of a chair."
-              style={{ width: "100%", height: "90vh", objectFit: "cover" }}
+              style={{ width: "100%", height: "95vh", objectFit: "cover" }}
               />
         
            <div className="top-header">
@@ -20,11 +21,23 @@ const HomepageHeader = () => {
                src={RedLogo} 
                alt="Art Institute of Chicago" 
                className="logo" />
+               
+              <div className="header-navbar">
 
               <div className="secondary-navbar">
                 <a href="#Buy Tickets">Buy Tickets</a>
                 <a href="#Become a member">Become a member</a>
                 <a href="#Shop">Shop</a>        
+             </div>
+             
+             <div className="primary-navbar">
+                <a href="#Visit">Visit</a>
+                <a href="#Exhibitions & Events">Exhibitions & Events</a>
+                <a href="#The Collection">The Collection</a> 
+                <FaSearch className="search-icon" />  
+             
+             </div>
+
              </div>
 
 

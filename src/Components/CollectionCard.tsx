cbcae45bf0e1765dd0fc049ecf05d2b810f314b8
Samabrita <samabritabiswas2302@gmail.com>
2023-10-30 15:53:41 +0530

    image failure issue

--- src/Components/CollectionCard.tsx
+++ src/Components/CollectionCard.tsx
@@ -3,7 +3,7 @@ import '../Styles/Artworks.css';
 
 interface CollectionCardProps {
   id: string;
-  imageId: string | null; // Make imageId nullable
+  imageId: string;
   title: string;
   artistTitle: string;
 }
@@ -24,7 +24,7 @@ const CollectionCard: React.FC<CollectionCardProps> = ({ id, imageId, title, art
     <div className='artwork-card' key={id}>
       {/* Image */}
       <img
-        src={`https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`} 
+        src={`https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg` ||  'https://artic-web.imgix.net/7b38c611-392c-422d-8eea-53be80ac65cd/EighthBlackbirdSaverioTruglia.jpg?rect=0%2C525%2C3204%2C1800&auto=format%2Ccompress&q=80&fit=crop&crop=faces%2Ccenter&w=1200&h=674'} 
         alt={title}
         className="card-image"
         loading='lazy'
